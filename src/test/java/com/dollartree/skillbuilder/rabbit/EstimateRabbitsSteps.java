1e0be7ead17b1778d7bfc8ac8df2b5d62a91b5bd
Bala Palanisamy <covaibala@gmail.com>
2019-09-07 00:11:42 +0530

    PASS Commit.

--- src/test/java/com/dollartree/skillbuilder/rabbit/EstimateRabbitsSteps.java
+++ src/test/java/com/dollartree/skillbuilder/rabbit/EstimateRabbitsSteps.java
@@ -2,7 +2,6 @@ package com.dollartree.skillbuilder.rabbit;
 
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Named;
-import org.jbehave.core.annotations.Pending;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 
